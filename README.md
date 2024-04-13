b5132bac3610756a1a164e16add4af0b0cc8f77a
cjuarez72 <116735739+cjuarez72@users.noreply.github.com>
2024-04-13 11:31:50 +0200

    Header y barra de busqueda estilos

---
+++ README.md
@@ -0,0 +1,1 @@
+# MealMarket-TFG
---
+++ src/Controller/defaultu.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Controller;
+
+use Exception;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use App\Controller\UserInterface;
+//Para poder usar archivos
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Filesystem\Path;
+
+use App\Controller\JsonResponse;
+
+
+class defaultu extends AbstractController{
+
+
+    #[Route('/defaults', name: 'defaults')]
+    public function defaults(){
+       return $this-> render('header.html.twig');
+    }
+}
