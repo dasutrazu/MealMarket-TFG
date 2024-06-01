7be9d60f05de2a68056f3a4fe5926205b96fea0a
cjuarez72 <116735739+cjuarez72@users.noreply.github.com>
2024-06-01 13:52:24 +0200

    avance del carrito

--- src/Controller/carritoController.php
+++ src/Controller/carritoController.php
@@ -37,6 +37,7 @@ class carritoController extends AbstractController{
     public function index(): Response
     {
         $usuario = $this->getUser();
+        dump($usuario);
         $carrito = $this->entityManager->getRepository(Carrito::class)->findOneBy(['id_user' => $usuario]);
         
         if (!$carrito) {
@@ -96,48 +97,106 @@ class carritoController extends AbstractController{
     } */
 
     #[Route('/addProducto/{idProduct}', name: 'addProducto')]
-public function addProducto(int $idProduct)
-{
-    $usuario = $this->getUser();
-    $carrito = $this->entityManager->getRepository(Carrito::class)->findOneBy(['id_user' => $usuario->getIdUser()]);
+    public function addProducto(int $idProduct, Request $request): Response
+    {
+        try {
+            $usuario = $this->getUser()->getIdUser();
+            $productoId = $request->request->get("id_producto");
 
-    if (!$carrito) {
-        $carrito = new Carrito();
-        $carrito->setIdUser($usuario);
-        $this->entityManager->persist($carrito);
-        $this->entityManager->flush(); // Guardar el carrito primero para obtener su ID
+            // Comprobamos si el producto existe
+            $producto = $this->entityManager->getRepository(Producto::class)->find($productoId);
+            if (!$producto) {
+                throw $this->createNotFoundException('The product does not exist');
+            }
+
+            dump($usuario); // Check if this dump statement is executed
+
+            $carrito = $this->entityManager->getRepository(Carrito::class)->findOneBy(['id_user' => $usuario]);
+
+            dump($carrito); // Check if this dump statement is executed
+
+            if ($request->isMethod('POST')) { // Check if the method is POST
+                if (!$carrito) { // If the cart does not exist
+                    $carrito = new Carrito();
+                    $carrito->setIdUser($usuario);
+                    $this->entityManager->persist($carrito);
+                    $this->entityManager->flush(); // Save the cart first to get its ID
+                }
+
+                // Check if product is already in the cart
+                $carritoProducto = $this->entityManager->getRepository(CarritoProducto::class)->findOneBy([
+                    'id_carrito' => $carrito, 
+                    'id_producto' => $producto
+                ]);
+
+                dump($carritoProducto); // Check if this dump statement is executed
+
+                if (!$carritoProducto) {
+                    $carritoProducto = new CarritoProducto();
+                    $carritoProducto->setIdCarrito($carrito);
+                    $carritoProducto->setIdProducto($producto);
+                    $carritoProducto->setCantidad(1);
+                    $this->entityManager->persist($carritoProducto);
+                } else {
+                    $carritoProducto->setCantidad($carritoProducto->getCantidad() + 1);
+                }
+
+                $this->entityManager->flush(); 
+            }
+
+            return $this->redirectToRoute('carrito.html.twig', [
+                'id_carrito' => $carrito,
+                'carritoProductos' => $carritoProducto
+            ]);
+        } catch (Exception $e) {
+            dump($e->getMessage()); // Dump any exception message
+            return $this->render('error.html.twig', ['message' => $e->getMessage()]);
+        }
+
+
+                /*
+        $usuario = $this->getUser();
+        $carrito = $this->entityManager->getRepository(Carrito::class)->findOneBy(['id_user' => $usuario->getIdUser()]);
+
+         if (!$carrito) {
+            $carrito = new Carrito();
+            $carrito->setIdUser($usuario);
+            $this->entityManager->persist($carrito);
+            $this->entityManager->flush(); // Guardar el carrito primero para obtener su ID
+        }
+
+        $producto = $this->entityManager->getRepository(Producto::class)->find($idProduct);
+
+        if (!$producto) {
+            throw $this->createNotFoundException('Producto no encontrado');
+        }
+
+        $carritoProducto = $this->entityManager->getRepository(CarritoProducto::class)->findOneBy(['id_carrito' => $carrito, 'id_producto' => $producto]);
+
+        if (!$carritoProducto) {
+            $carritoProducto = new CarritoProducto();
+            $carritoProducto->setIdCarrito($carrito);
+            $carritoProducto->setIdProducto($producto);
+            $carritoProducto->setCantidad(1);
+            $this->entityManager->persist($carritoProducto);
+        } else {
+            $carritoProducto->setCantidad($carritoProducto->getCantidad() + 1);
+        }
+
+        $this->entityManager->flush();
+        dump($carrito);
+        dump($carritoProducto); */
+        
+        // Actualizar la lista de productos del carrito y el total
+
+        //return $this->redirectToRoute('app_carrito');
+    /* return $this->redirectToRoute('carrito.html.twig', [
+            'id_carrito' => $carrito,
+            'carritoProductos' => $carritoProducto
+        ]);*/
+
+
     }
-
-    $producto = $this->entityManager->getRepository(Producto::class)->find($idProduct);
-
-    if (!$producto) {
-        throw $this->createNotFoundException('Producto no encontrado');
-    }
-
-    $carritoProducto = $this->entityManager->getRepository(CarritoProducto::class)->findOneBy(['id_carrito' => $carrito, 'id_producto' => $producto]);
-
-    if (!$carritoProducto) {
-        $carritoProducto = new CarritoProducto();
-        $carritoProducto->setIdCarrito($carrito);
-        $carritoProducto->setIdProducto($producto);
-        $carritoProducto->setCantidad(1);
-        $this->entityManager->persist($carritoProducto);
-    } else {
-        $carritoProducto->setCantidad($carritoProducto->getCantidad() + 1);
-    }
-
-    $this->entityManager->flush();
-    dump($carrito);
-    dump($carritoProducto);
-    
-    // Actualizar la lista de productos del carrito y el total
-
-    return $this->redirectToRoute('app_carrito');
-   /* return $this->redirectToRoute('carrito.html.twig', [
-        'id_carrito' => $carrito,
-        'carritoProductos' => $carritoProducto
-    ]);*/
-}
 
 
 }
