2ce4f247b1069edc5ac3f3591a8ba5e3380092cc
Rubén Ramos Espeso <rubenramosespeso@gmail.com>
2024-06-09 09:15:19 +0200

    added pagina de perfil

---
+++ src/Controller/usuarioController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Pedido;
+use App\Entity\Carrito;
+use App\Entity\CarritoProducto;
+use App\Entity\PedidoProducto;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class usuarioController extends AbstractController
+{
+    #[Route('/perfil', name: 'app_perfil')]
+    public function perfil(EntityManagerInterface $entityManager): Response
+    {
+        
+        $usuario = $this->getUser();
+        $pedidos = $entityManager->getRepository(Pedido::class)->findBy(['id_user' => $usuario]);
+        return $this->render("perfil.html.twig", ["usuario" => $usuario, "pedidos"=>$pedidos]);
+    }
+
+
+    #[Route('/pedido/{id_pedido}', name: 'app_pedido')]
+    public function pedido($id, EntityManagerInterface $entityManager){
+
+        $pedido = $entityManager->getRepository(Pedido::class)->findOneBy(['id_pedido'=> $id]);
+        $productosPedido = $entityManager->getRepository(PedidoProducto::class)->findBy(['id_pedido'=>$pedido]);
+        //return $this->render("", ["productos"=>$productosPedido]);
+
+    }
+
+}
